ea2e6c67d2fe13f039216a5a5c28abe588009cea
Yonaykel <yonaykel.rivas2008@gmail.com>
2026-09-06 16:23:43 -0600

    fear: menu de PrincipalCuenta

--- PrjCuenta/PrincipalCuenta.java
+++ PrjCuenta/PrincipalCuenta.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import java.lang.String;
 public class PrincipalCuenta{
     public static void main(String args){    
         String opcion = "";
+        Scanner scan = new Scanner(System.in);
         do{
+            mostrarMenu();
+            opcion = "10";
             
             
         
@@ -10,8 +14,10 @@ public class PrincipalCuenta{
     }
     private static void mostrarMenu(){
         System.out.println("====Menu====\n");
-        System.out.print("1. Crear cuenta\n2. Conocer la cantidad cuentas creadas\n3. Listas cuentas\n");
-        System.out.print("");
+        System.out.print("1. Crear cuenta\n2. Conocer la cantidad cuentas creadas\n3. Listar cuentas\n");
+        System.out.print("4. Seleccionar cuenta actual\n5. Asignar nombre de la cuenta habiente\n");
+        System.out.print("6. Depositar\n7.Retirar\n8.Consultar saldo\n9. Consultar estado de cuenta\n10.Salir\n");
+        System.out.println("Opcion: ");
     }
 
     
